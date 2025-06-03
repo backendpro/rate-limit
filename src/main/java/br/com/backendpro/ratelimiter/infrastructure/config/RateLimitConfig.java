ecc7346541d50c381b8831f4520f7116d1d09f34
backendpro <devcaiorodrigues@gmail.com>
2025-06-02 23:44:50 -0300

    feat: prometheus and logic adjust

--- src/main/java/br/com/backendpro/ratelimiter/infrastructure/config/RateLimitConfig.java
+++ src/main/java/br/com/backendpro/ratelimiter/infrastructure/config/RateLimitConfig.java
@@ -1,8 +1,10 @@
 package br.com.backendpro.ratelimiter.infrastructure.config;
 
+import io.github.resilience4j.micrometer.tagged.TaggedRateLimiterMetrics;
 import io.github.resilience4j.ratelimiter.RateLimiter;
 import io.github.resilience4j.ratelimiter.RateLimiterConfig;
 import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,16 +14,20 @@ import java.time.Duration;
 public class RateLimitConfig {
 
     @Bean
-    public RateLimiter rateLimiter() {
+    public RateLimiter rateLimiter(MeterRegistry meterRegistry) {
+        RateLimiterConfig config = RateLimiterConfig.custom()
+                .limitForPeriod(5)
+                .limitRefreshPeriod(Duration.ofSeconds(1))
+                .timeoutDuration(Duration.ofMillis(500))
+                .build();
 
-        RateLimiterRegistry registry =
-                RateLimiterRegistry.of(RateLimiterConfig
-                        .custom()
-                        .limitForPeriod(5)
-                        .limitRefreshPeriod(Duration.ofSeconds(1))
-                        .timeoutDuration(Duration.ofMillis(500))
-                        .build());
+        RateLimiterRegistry registry = RateLimiterRegistry.of(config);
+        RateLimiter rateLimiter = registry.rateLimiter("rateLimiter");
 
-        return registry.rateLimiter("rateLimiter");
+        TaggedRateLimiterMetrics
+                .ofRateLimiterRegistry(registry)
+                .bindTo(meterRegistry);
+
+        return rateLimiter;
     }
 }
